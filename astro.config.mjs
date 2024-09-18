e64827ed967cca3f4fd67efc1f264937eac876da
icorbachodev <ismaelcorbachodev@gmail.com>
2024-09-18 18:46:47 +0200

    modified: Added concerts collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [],
+  content: {
+    collections: {
+      concerts: {
+        schema: {
+          title: 'string',
+          date: 'string',
+          location: 'string',
+          image: 'string',
+          description: 'string'
+        }
+      }
+    }
+  }
+});
